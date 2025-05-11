1f1381ae37c535f53a762abf74e3f900e25e8abb
imamkoo <imammm.kooo@gmail.com>
2025-05-11 17:34:03 +0700

    remove card

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -29,19 +29,6 @@ export function Login() {
           </p>
         </div>
         
-        <Card className="mb-6 border-brand-light-purple">
-          <CardContent className="pt-6">
-            <Alert className="bg-blue-50 border-blue-200">
-              <InfoIcon className="h-4 w-4 text-blue-500" />
-              <AlertDescription className="text-sm text-blue-700">
-                <strong>Admin Login:</strong><br />
-                Email: admin@gmail.com<br />
-                Password: Admin1@ (auto-created)
-              </AlertDescription>
-            </Alert>
-          </CardContent>
-        </Card>
-        
         <AuthForm mode="login" />
       </div>
     </div>
